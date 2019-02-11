94e1bb14353098b489d8b1ebae069a862ae50c3c
Poornima <poornimaln@gmail.com>
2019-02-12 08:54:09 +1100

    MapMyWorld code

---
+++ catkin_ws_rtabmap3/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/catkin_ws_rtabmap3/src
+# Build directory: /home/robond/catkin_ws_rtabmap3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(rtab_kinnect_bot)
---
+++ catkin_ws_rtabmap3/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/robond/catkin_ws_rtabmap3/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/robond/catkin_ws_rtabmap3/devel;/home/robond/catkin_ws_per/devel;/home/robond/catkin_w_per/devel;/opt/ros/kinetic".split(';') if "/home/robond/catkin_ws_rtabmap3/devel;/home/robond/catkin_ws_per/devel;/home/robond/catkin_w_per/devel;/opt/ros/kinetic" != "" else []
